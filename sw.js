cc4291a20e0086c5713894a9d485776ef70bf94c
rpint <otoa@live.cn>
2020-08-30 10:21:09 +0800

    启用service worker成功

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ var staticImageCacheName = "image" + cacheVersion;
 var staticAssetsCacheName = "assets" + cacheVersion;
 var contentCacheName = "content" + cacheVersion;
 var vendorCacheName = "vendor" + cacheVersion; 
-var maxEntries = 100; /* ��󻺴����� */
-var maxAgeSeconds = 60*60*24; /* ��󻺴�ʱ�䣬��λ(s) */
+var maxEntries = 100; /* 最大缓存数量 */
+var maxAgeSeconds = 60*60*24; /* 最大缓存时间，单位(s) */
 self.toolbox.router.get("/(.*)", self.toolbox.networkFirst, {
     origin: /noscos.nos-eastchina1\.126\.net/,
     cache: {
@@ -20,7 +20,17 @@ self.toolbox.router.get("/(.*)", self.toolbox.networkFirst, {
        maxEntries: maxEntries
 }
   });
-self.toolbox.router.get("/css/(.*)", self.toolbox.networkFirst, {origin: /cdn\.jsdelivr\.net/,});
-self.toolbox.router.get("/js/(.*)", self.toolbox.networkFirst, {origin: /cdn\.jsdelivr\.net/,});
-self.toolbox.router.get("/fonts/(.*)", self.toolbox.networkFirst, {origin: /cdn\.jsdelivr\.net/,});
-self.toolbox.router.get("/images/(.*)", self.toolbox.networkFirst, {origin: /cdn\.jsdelivr\.net/,});
+
+/* 缓存cdn静态资源 */
+self.toolbox.router.get("/gh/(.*)", self.toolbox.fastest, {origin: /cdn\.jsdelivr\.net/,});
+
+
+/* NoCache */
+self.toolbox.router.get("/sw.js",self.toolbox.networkFirst);
+
+self.addEventListener("install",
+function(event) {return event.waitUntil(self.skipWaiting())
+});
+self.addEventListener("activate",
+function(event) {return event.waitUntil(self.clients.claim())
+})
